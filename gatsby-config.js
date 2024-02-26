e3d515cfedb0c27535b5e6c4e2d96c043d32a630
jtse0316 <jtse0316@gmail.com>
2024-02-26 04:55:48 +0000

    used image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `gatsby-tutorial`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
+const Static = () => {}
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm following a Gatsby tutorial.</p>
+      <StaticImage
+        alt="Kakshi with double mangekyo sharingan"
+        src="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/07/kamui_lightning_cutter.jpg"
+      />
     </Layout>
   )
 }
